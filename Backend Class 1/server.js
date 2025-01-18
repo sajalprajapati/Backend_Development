ff4a12fe230130b63bd6d081ff004bf86229b714
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2025-01-18 07:29:34 +0000

    MongoDB set up learning is being done..I have expalined it in my code ..

--- Backend Class 1/server.js
+++ Backend Class 1/server.js
@@ -2,38 +2,40 @@
 //Server Initiated:
 
 
-import bodyParser from 'body-parser'; //<<<-----Step 1:to install middleware....
-import express, { request, respond } from 'express'; 
-const app=express();
+// Server Initiated:
+import bodyParser from 'body-parser'; // <<<----- Step 1: to install middleware....
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
 
+// Now we are using the concept of middleware:
+app.use(bodyParser.json()); // Specifically parse JSON data and add it to the request.body object
 
-//now we are using the concept of middleware:
-app.use(bodyParser.json()); //specifically parse JSON data and add it to the reuqest.body object
+// Pehla route bana rhe hai...
+app.get('/', (request, response) => {
+  response.send("Hello Ji! Kaise Hai Saare");
+});
 
+app.post('/api/cars', (request, response) => {
+  const { name, brand } = request.body;
+  console.log(name);
+  console.log(brand);
+  response.send("Car Submitted Successfully");
+});
 
-//pehla route bana rhe hai...
-app.get('/',(request ,respond)=>{respond.send("Hello Ji! Kaise Hai Saare")});
-app.post
-('/api/cars',(request,respond)=>
- {
-   const {name,brand}=request.body;
-   console.log(name);
-   console.log(brand);
-   respond.send("Car Submitted Successfully");
- }
-)
+// Activate the server on 3000 port
+app.listen(3000, () => {
+  console.log("Server Chal gya hai bhai at 3000");
+});
 
-
-
-
-
-//activate the server on 3000 port 
-app.listen(3000,() =>{console.log("Server Chal gya hai bhai at 3000")});
-
-
-
-
-
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/') //<-----here is the mistakeee ..i think it is disconnected
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 
 /*

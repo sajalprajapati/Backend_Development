d1ba173fd94ea98647e3ac959c85d2b599aa6d41
sajalprajapati <prajapatisajal00@gmail.com>
2025-02-05 10:49:58 +0530

    Learning to apply cloudinary.

--- Backend Class 6/FormHandling class 1/controllers/fileUpload.js
+++ Backend Class 6/FormHandling class 1/controllers/fileUpload.js
@@ -2,26 +2,36 @@ import File from "../models/File.js";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+
+//if we use require method then i don't need to do above import....
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Local File Upload Handler Function
 export const localFileUpload = async (req, res) => {
     try {
-        // Fetching the file
+        // Step 1: Fetching the file from request.
         const file = req.files.file;
         console.log("File received:", file);
+        // console.log(file);
 
-        // Define storage path
+
+
+
+        // Step 2:  Define storage path
         let path = `${__dirname}/files/${Date.now()}.${file.name.split('.').pop()}`;
-        console.log("File will be saved at:", path);
+        // console.log("File will be saved at:", path);
 
-        // Move file to the specified location
+        //Step 3:  Move file to the specified location
         await file.mv(path);
+
+
 
         res.json({
             success: true,
             message: "Local File Uploaded Successfully"
         });
+
+
 
     } catch (error) {
         console.log("Error while uploading:", error);
